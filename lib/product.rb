dc628419dcf7572bccfbcb31b688e6776f6e367e
Kiera Allen <kieraaallen@gmail.com>
2020-03-25 10:34:59 -0600

    Geez, Louise what a doozy... Add and test that @is_hoarded default is false, and add/test product is hoarded by .hoard method

--- lib/product.rb
+++ lib/product.rb
@@ -9,9 +9,19 @@ class Product
     @name = name
     @unit_price = unit_price
     @quantity = quantity
+    @is_hoarded = false
   end
 
   def total_price
     @unit_price * (@quantity).to_f
   end
+
+  def is_hoarded?
+    @is_hoarded
+  end
+
+  def hoard
+    @is_hoarded = true
+  end
+
 end
