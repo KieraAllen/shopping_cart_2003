fb76de2e559cc6949ea72077ea30ed95b8f93a35
Kiera Allen <kieraaallen@gmail.com>
2020-03-25 09:45:27 -0600

    Test total_price method and change 3.70 in Product initialize method to 'unit_price'

--- lib/product.rb
+++ lib/product.rb
@@ -7,7 +7,7 @@ class Product
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
-    @unit_price = 3.70
+    @unit_price = unit_price
     @quantity = quantity
   end
 
